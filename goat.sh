4227f874c4713e6d529dd14b40f6476d40e32bae
tetrider <tetrider91@gmail.com>
2018-11-10 03:54:45 +0800

    refactoring 2

--- goat.sh
+++ goat.sh
@@ -5,37 +5,42 @@ goat(){
 key=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 24 | head -n 1)
 # Main list of commands
 commands_line=$(cat <<EOF
-for var in oneiter; do
-    echo '****************************';
+for fakeiter in fakecycle; do
+    echo '***';
     cat /etc/redhat-release 2>/dev/null || cat /etc/os-release | grep -Po '(?<=PRETTY_NAME=\").*(?=\")' 2>/dev/null || echo 'Unknown OS';
-    uptime; echo;
-    df -h; echo;
+    uptime; 
+    echo;
+    df -h | grep -E '(8.%)|(9.%)|100%'; 
     free -m;
-    echo '****************************';
-    cd /usr/local/mgr5 2>/dev/null || { echo 'NO mgr5 directory'; break; };
-    echo 'bin/CORE 1>/dev/null' | tr '[:upper:]' '[:lower:]' | sh;
+    echo '***';
+    cd /usr/local/mgr5 || break;
+    sbin/mgrctl mgr 1>/dev/null || break;
     sbin/mgrctl mgr | cut -f2 -d= | while read i; do 
-        echo "bin/CORE \\\$i -i" | tr '[:upper:]' '[:lower:]' | sh;
+        echo "bin/coRE \\\$i -i" | sed 's/RE/re/' | sh;
     done;
     echo;
-    sbin/ihttpd || echo 'ihttpd ERROR';
+    sbin/ihttpd || break;
     echo;
     echo 'ps -f -C coRE' | sed 's/RE/re/' | sh;
     echo;
     sbin/mgrctl mgr | cut -f2 -d= | sed '/\(mini\|node\)$/d' | while read mgr; do 
         sbin/mgrctl -m \\\$mgr session.newkey key=$key; 
         sbin/ihttpd | cut -f3 -d: | sed '/80/d' | sort | uniq | while read port; do
-            echo 'https://'$1':'\\\$port'/'\\\$mgr'?func=auth&key='$key; 
+            echo https://$1:\\\$port/\\\$mgr?func=auth\&key=$key; 
         done;
     done;
 done;
+alias less="less -R";
 EOF
 ) 
 # Copy command and authkey to clipboard 
 # echo "?func=auth&key=$key" | xsel -ib 2>/dev/null || echo "Failed copy key to clipboard. Please install xsel"
 
+# Open default CP in browser
+# xdg-open "https://$1:1500?func=auth&key=$key"
+
 # Go. If error try without commands
-echo $commands_line
+# echo $commands_line
 ssh sup@ssh -t "go $@ -t \"$commands_line bash -l\"" || ssh sup@ssh -t "go $@"; 
-# ssh $@ -t "$commands bash -l" 
+# ssh $@ -t "$commands_line bash -l" 
 }
