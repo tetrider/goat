2262815eea1c4abcde7de868542d151dba2127d5
tetrider <tetrider91@gmail.com>
2018-12-13 20:16:59 +0800

    new sysinfo format with colors

--- goat.sh
+++ goat.sh
@@ -6,14 +6,38 @@ goat(){
 
     # Main list of commands
     commands_line=$(cat <<EOF
+    RED='\033[0;31m';
+    YELLOW='\033[1;33m';
+    GREEN='\033[0;32m';
+    GRAY='\033[1;30m';
+    NC='\033[0m';
+    div=\$(for i in {1..60}; do printf '*'; done);
     for fakeiter in fakecycle; do
-        echo '***';
+        echo -e "\${GRAY}\${div}\${NC}";
+        printf "\${GRAY}OS: \${NC}";
         cat /etc/redhat-release 2>/dev/null || cat /etc/os-release | grep -Po '(?<=PRETTY_NAME=\").*(?=\")' 2>/dev/null || echo 'Unknown OS';
-        uptime; 
-        echo;
-        df -h | grep -E '(8.%)|(9.%)|100%'; 
-        free -m;
-        echo '***';
+        printf "\${GRAY}Uptime: \${NC}";
+        uptime -p | sed 's/up //'; 
+        printf "\${GRAY}Load average: \${NC}";
+        uptime | grep -Po '(?<=load average: ).*';
+        if [[ ! -z \$(df -lh | grep -E '(8.%)|(9.%)|100%') ]]; then
+            echo -e "\${GRAY}Disk usage:";
+            printf "\${GREEN}";
+            df -lh | grep -E '(8.%)' | awk '{print \$5, "used on", \$6}'; 
+            printf "\${YELLOW}";
+            df -lh | grep -E '(9.%)' | awk '{print \$5, "used on", \$6}'; 
+            printf "\${RED}";
+            df -lh | grep -E '100%' | awk '{print \$5, "used on", \$6}';
+            printf "\${NC}";
+        fi; 
+        freemem=\$(free -mt | grep Total | awk '{print \$4}');
+        totalmem=\$(free -mt | grep Total | awk '{print \$2}');
+        printf "\${GRAY}Memory free: \${NC}";
+        if (( "\$freemem" < "100" )); then
+            printf "\${RED}";    
+        fi;
+        echo -e "\${freemem}/\${totalmem}\${NC}";
+        echo -e "\${GRAY}\${div}\${NC}";
         cd /usr/local/mgr5 || break;
         sbin/mgrctl mgr 1>/dev/null || break;
         sbin/mgrctl mgr | cut -f2 -d= | while read i; do 
